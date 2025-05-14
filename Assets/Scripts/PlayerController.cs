9f7730ea8f4e768574a4120dd28820132844c5e0
alierenkarateke <alierenkarateke.offical@gmail.com>
2025-05-15 01:14:16 +0300

    V0.1 Update
    Player Controller,
    Camera Movement,
    Looping Background added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Hız, fizik ve yön için değişkenler oluşturduk
+    [SerializeField]float playerSpeed;
+    private Rigidbody2D rb;
+    private Vector2 playerDirection;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rb'yi playerin sahip olduğu component'e bağladık
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Y ekseninde hareket etmek için input atadık
+        float directionY = Input.GetAxisRaw("Vertical");
+
+        // Oyuncunun yönünü belirten vectör'ü tanımladık
+        playerDirection = new Vector2(0, directionY).normalized;        
+    }
+
+    void FixedUpdate()
+    {
+        // Oyuncuya hız verdik
+        rb.velocity = playerDirection * playerSpeed;
+    }
+}
